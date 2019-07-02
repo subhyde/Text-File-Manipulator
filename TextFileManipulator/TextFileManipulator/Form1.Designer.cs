859cc8a2705938fb81c6c02a62d09d3165f12cd8
subhyde <ethan_johnson@hotmail.ca>
2019-07-02 15:18:04 -0700

    Add files via upload
    initial commit

---
+++ TextFileManipulator/TextFileManipulator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TextFileManipulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // richTextBox2
+            // 
+            resources.ApplyResources(this.richTextBox2, "richTextBox2");
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.RichTextBox2_TextChanged);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ TextFileManipulator/TextFileManipulator/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace TextFileManipulator
+{
+    
+    public partial class Form1 : Form
+    {
+        public string filePath = null;
+        public Form1()
+        {
+
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+    
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog getpath = new OpenFileDialog();
+            if (getpath.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = getpath.FileName;
+            }
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (filePath != null)
+            {
+                richTextBox1.Text = string.Empty; //sets the text box to empty so their isnt anything prior 
+                System.IO.StreamReader objReader; // initilizes the reader
+                objReader = new System.IO.StreamReader(filePath); 
+                richTextBox1.Text = objReader.ReadToEnd(); //displays txt file
+                objReader.Dispose(); // frees memory
+            }
+            else
+            {
+                richTextBox1.Text = "Please select a text file";
+
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            string dmessage = "Please enter your text above and cick the button again";
+            if (richTextBox2.Text == string.Empty)
+            {
+                richTextBox1.Text = dmessage;
+            }
+            
+            else if (filePath == null)
+            {
+                richTextBox1.Text = "Please select a text file";
+            }
+            else {
+                StreamWriter writer = new StreamWriter(filePath, true);
+                writer.WriteLine(richTextBox2.Text);
+                writer.Dispose();
+                richTextBox2.Text = string.Empty;
+                
+
+            }
+
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if(richTextBox2 == null)
+            {
+                richTextBox1.Text = "please enter your word to search in this text box above";
+            }
+            else if(filePath == null)
+            {
+                richTextBox1.Text = "Please select a text file";
+            }
+            else
+            {
+                richTextBox1.Text = string.Empty;
+                List<string> found = new List<string>();
+                string line;
+                using (StreamReader file = new StreamReader(filePath))
+                {
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        if (line.Contains(richTextBox2.Text))
+                        {
+                            richTextBox1.Text += line + "\r\n";
+                            
+                        }
+                    }
+                   
+                }
+            }
+
+        }
+
+        private void RichTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+            richTextBox2.Text = string.Empty;
+        }
+    }
+}
